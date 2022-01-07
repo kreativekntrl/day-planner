1c576f91ccc692001599b31bf24cf5527840af6d
Ava <avamsteimle@gmail.com>
2022-01-07 15:45:19 -0800

    final updateDiv() changes

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ var hour15 = $("#hour-15");
 var hour16 = $("#hour-16");
 var hour17 = $("#hour-17");
 var textBox;
-var timeDiv;
-
-var timeArray = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
+var timeDivId;
 
 //on planner open current day is displayed 
     //moment function updates a header with date
@@ -20,72 +18,33 @@ $("#currentDay").text(currentDate);
 
 //holding current hour to store text when user clicks saveBtn
 saveBtn.on("click", function () {
-    timeDiv = $(this).parent().attr("id");
+    timeDivId = $(this).parent().attr("id");
     textBox = $(this).siblings(".description").val();
     storeText();
 })
 
 //Saves user input and stores to local storage 
-//loops through text inputs and stores all text
+//happens when save button is clicked
 function storeText(){
     localStorage.setItem(timeDiv, textBox);
 };
 
-//Updates current time 
-
-//Loops through our array of times and updates the color of the div
-/*function updateDiv () {
-    for (var i = 0; i < timeArray.length; i++) {
-        if (parseInt(timeArray[i].text()) === parseInt(timeArray[4].text())){
-            timeArray[i].addClass("present");
-        } else if (parseInt(timeArray[i].text()) < parseInt(timeArray[4].text())){
-            timeArray[i].addClass("past");
-        } else {
-            timeArray[i].addClass("future");
-        }
-    }
-}*/
-
-var nineAM = moment("9AM", "hA").format("hA");
-var tenAM = moment("10AM", "hA").format("hA");
-var elevenAM = moment("11AM", "hA").format("hA");
-var noon = moment("12PM", "hA").format("hA");
-var onePM = moment("1PM", "hA").format("hA");
-var twoPM = moment("2PM", "hA").format("hA");
-var threePM = moment("3PM", "hA").format("hA");
-var fourPM = moment("4PM", "hA").format("hA");
-var fivePM = moment("5PM", "hA").format("hA");
-
-var hourArray = [nineAM, tenAM, elevenAM, noon, onePM, twoPM, threePM, fourPM, fivePM];
-
-var currentTime = moment("10AM", "hA").format("hA");
+//Updates current time div and changes colors
+var currentHour = moment().format("H");
+currentHour = parseInt(currentHour);
 
 function updateDiv() {
-    for(j=i=0; i < timeArray.length && j < hourArray.length; i++, j++) {
-        if (parseInt(timeArray[i].text()) === parseInt(currentTime)) {
-            timeArray[i].addClass("present");
-        }
-        if (parseInt(hourArray[j]) < parseInt(currentTime) && parseInt(timeArray[i].text()) < parseInt(currentTime)){
-            timeArray[i].addClass("past");
-        }
-        if (parseInt(timeArray[i].text()) > parseInt(currentTime) && parseInt(hourArray[j]) > parseInt(currentTime)){
-            timeArray[i].addClass("future");
-        }
+    for (var hour = 9; hour <= 17; hour++) {
+      var div = $("#hour-" + hour);
+      if (hour < currentHour) {
+        div.addClass("past");
+      } else if (hour === currentHour) {
+        div.addClass("present");
+      } else {
+        div.addClass("future");
+      }  
     }
 }
-
-/*function updateDiv() {
-    for(j=i=0; i < timeArray.length && j < hourArray.length; i++, j++) {
-        if (parseInt(timeArray[i].text()) === parseInt(currentTime)){
-            timeArray[i].addClass("present");
-            console.log(currentTime);
-        }
-        if (parseInt(timeArray[i].text()) != parseInt(currentTime)){
-            timeArray[i].addClass("past");
-        }
-        /*if (parseInt(timeArray[i].text()) > parseInt(currentTime)){
-            timeArray[i].addClass("future");
-        }*/
-
-
 updateDiv();
+
+
